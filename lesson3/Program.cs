ad5254433a55b6672ccdf16cc9efe1650af4d7a0
CORBY <curtisjeckan@mail.ru>
2022-03-12 09:08:06 +0500

    задание 3.1

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -1,7 +1,23 @@
 ﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        int next_string = 0;
+        int[,] array = new int[5, 5];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = next_string + 1;
+                Console.WriteLine($"{new string(' ', next_string)}{array[i, j]}");
+                next_string++;
+            }
+        }
+    }
 
+}
 
-//практическую работу номер 3 выполню до 16 марта 2022, извините...
 
 
 
